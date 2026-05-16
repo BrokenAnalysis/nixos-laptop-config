2360abca4f6d263586a53150fec9588ae84d419d
Charlie Madsen <madsenchas@gmail.com>
2026-05-15 20:45:54 -0500

    rebuild: 2026-05-15 20:45

--- configuration.nix
+++ configuration.nix
@@ -74,8 +74,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     #jack.enable = true;
+  };
+
  services.ollama = {
    enable = true;
    loadModels = [ "deepseek-r1:latest" ];
@@ -86,7 +87,7 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
+  
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
